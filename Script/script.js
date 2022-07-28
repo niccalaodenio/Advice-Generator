ddf65607454cabe9092baecf10f8a7c179607228
NICCA <niccalaodenio26@gmail.com>
2022-07-28 15:52:27 +0800

    initial commit

--- Script/script.js
+++ Script/script.js
@@ -7,11 +7,23 @@ async function fetch_Data(){
     let res = await fetch('https://api.adviceslip.com/advice')
     let data = await res.json()
     //return data
+    
+    updateUI(data);
+    console.log("yey!")
+}
+
+let updateUI = data=>{
+    // con.innerHTML = `
+    // <div id="advice-num"> Advice #${data.slip.id}</div>
+    // <div class="advice"> <q>${data.slip.advice}</q> </div>
+    // <img class="pattern" src="images/pattern-divider-desktop.svg" alt="">
+    // <button class="btn"><img class="btn" src="images/icon-dice.svg" alt=""></button>
+
+    // `
     let advice_num = con.firstElementChild ;
     let advice = con.firstElementChild.nextElementSibling;
     let num = `Advice #${data.slip.id}`
     let ad = `<q>${data.slip.advice}</q>`
     advice_num.innerHTML = num
     advice.innerHTML = ad
-    console.log("yey!")
 }
